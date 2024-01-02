7042eb9531ebcc0e78ea4c7f9198b402002e688e
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-02 17:25:04 +0000

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,3 +17,15 @@ from models.user import User
 def test_status():
     '''Test the status of the route'''
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def clases_count():
+    '''endpoint that retrieves the number of each objects by type'''
+    new_dict = {}
+    new_dict["amenities"] = storage.count("Amenity")
+    new_dict["cities"] = storage.count("City")
+    new_dict["places"] = storage.count("Place")
+    new_dict["reviews"] = storage.count("Review")
+    new_dict["states"] = storage.count("State")
+    new_dict["users"] = storage.count("User")
+    return jsonify(new_dict)
